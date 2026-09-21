12f272f18d7859ac9180a883b237c6a8eeb051b1
Miles <elijahtuinstra@gmail.com>
2026-09-20 18:35:34 -0600

    Make some minor fixes

--- player.gd
+++ player.gd
@@ -11,10 +11,8 @@ var double_jumped : bool = false
 var wall_jump_timer : float = 0.0
 
 @onready var animated_sprite = $PlayerSprites
-@onready var StillLight = $StillLight2D
 
 func _physics_process(delta: float) -> void:
-	StillLight.energy = 2.0
 	
 	if wall_jump_timer > 0:
 		wall_jump_timer -= delta
